b4f9c8a5c5793ba18d9aa768edcaef9c7b5ebd68
HarshitRuwali <harshitruwali@outlook.com>
2019-12-19 18:09:06 +0530

    Update voice_assistant.py

--- voice_assistant.py
+++ voice_assistant.py
@@ -16,7 +16,7 @@ import youtube_dl
 import wikipedia
 import smtplib
 from time import strftime
-import random
+#import random
 import requests
 import json as simplejson
 from bs4 import BeautifulSoup as soup
@@ -30,29 +30,69 @@ import email
 import imaplib
 import pandas as pd
 from googlesearch import *
+import pyttsx3
+import wolframalpha
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+
+'''
+engine = pyttsx3.init()
+sound = engine.getProperty('voices')
+engine.setProperty('voice', sound[32].id)
+engine.say("hello I am the first test case")
+engine.runAndWait()
+'''
 
 def botResponse(audio):
     "speaks audio passed as argument"
     print(audio)
+    engine = pyttsx3.init()
+    sound = engine.getProperty('voices')
+    engine.setProperty('voice', sound[33].id) #10 17 18 28 32 33 36 37 40
+    #engine.say("hello I am the first test case")
+   
     for line in audio.splitlines():
-        os.system("say " + audio)
-
-
+        #os.system("say " + audio)
+        engine.say(audio)
+    engine.runAndWait()
+        
+        
 def myCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        botResponse("How can I help you now?")
-        r.pause_treshold = 1
-        r.adjust_for_ambient_noise(source, duration = 1)
-        audio = r.listen(source)
+        #print("How can I help you now?")
+        #r.pause_treshold = 1
+        #r.adjust_for_ambient_noise(source, duration = 1) # duration was earlier 1
+        #audio = r.listen(source)
+        audio = r.listen(source, phrase_time_limit = 5)  
     try:
         command = r.recognize_google(audio).lower()
         print("you said: "+ command + "\n")
         
     except sr.UnknownValueError:
-        print("Sorry, Cant understand, Pls say again")
+        botResponse("Sorry, Cant understand, Please say again")
         command = myCommand()
     return command
+    '''
+    q = sr.Recognizer()
+    t = 0
+    with sr.Microphone() as source:
+        print("How can I help you now?")
+        while t==0:
+            audio = q.listen(source)
+            try:
+                command = q.recognize_google(audio)
+                print('you said :{}'.format(text))
+                #print("you said: "+command+"\n")
+                t=1
+            except:
+               botResponse("Sorry, Cant understand, Please say again")
+               t==0
+               command = myCommand();
+    return command
+    '''
 
 def assistant(command):
     "if statements for executing commands"
@@ -60,12 +100,16 @@ def assistant(command):
     if 'hello' in command:
             day_time = int(strftime('%H'))
             if day_time < 12:
-                botResponse('Hello Good morning, Sir')
+                botResponse('Hello, Good morning, Sir')
             elif 12 <= day_time < 18:
-                botResponse('Hello Good afternoon, Sir')
+                botResponse('Hello, Good afternoon, Sir')
             else:
-                botResponse('Hello Good evening, Sir')
-    
+                botResponse('Hello, Good evening, Sir')
+
+    #asking when you were created?         
+    elif 'created' in command:
+        botResponse('I was created on 15 December 2019 in the room of VIT  Bhopal University')
+
     #opening a webpage
     elif 'open' in command:
         reg_ex = re.search('open (.+)', command)
@@ -77,12 +121,29 @@ def assistant(command):
             botResponse("Opening " + domain)
         else:
             pass
+
+    #how are you and basic conversations
+    elif 'how are you' in command:
+        botResponse('I am great. Hoping the same for you.')   
+
+    elif 'How do you function' in command:
+        botResponse('I am working by the program written by Harshit Ruwali')
+
+    elif 'fuck off' or 'fuck you' in command:
+        botResponse('Fuck you too. Mother FUCKER.')
+    elif 'yash saxena' in command:
+        botResponse('YASH Saxena is a son of a BITCH. MOTHER-FUCKER')
+    elif '149' in command:
+        botResponse('The room of Vineet, he is an asshole')
+    elif 'vineet' in command:
+        botResponse('He is an total asshole, MOTHER-FUCKER')
+
         
     #telling you the current time 
     elif 'time' in command:
-         import datetime
-         now = datetime.datetime.now()
-         botResponse('Current time is %d hours %d minutes' % (now.hour, now.minute))
+        import datetime
+        now = datetime.datetime.now()
+        botResponse('Current time is %d hours %d minutes' % (now.hour, now.minute))
          
     
     #reading an email
@@ -139,27 +200,47 @@ def assistant(command):
 
     #opening the desired application in the Mac
     elif 'launch' in command:
-        reg_ex = re.search('launch(.*)', command)
+        reg_ex = re.search('launch (.*)', command)
         if reg_ex:
             appname = reg_ex.group(1)
             appname1 = appname+".app"
-            subprocess.call(["open","-n","/Applications"+appname1], stdout=subprocess.PIPE)
-            botResponse("I have opened the application")
+            subprocess.Popen(["open", "-n", "/Applications/" + appname1], stdout=subprocess.PIPE)
+            botResponse('I have launched the desired application')
         else:
             botResponse('The applicaton does not exixst')
         
     # to tell you the temprature of a perticular city
     elif 'current weather' in command:
-         reg_ex = re.search('current weather in (.*)', command)
-         if reg_ex:
-             city = reg_ex.group(1)
-             owm = OWM(API_key='221642e84fc28ddb1c3172447bbf1a39')
-             obs = owm.weather_at_place(city)
-             w = obs.get_weather()
-             k = w.get_status()
-             x = w.get_temperature(unit='celsius')
-             botResponse('Current weather in %s is %s. The maximum temperature is %0.2f and the minimum temperature is %0.2f degree celcius' % (city, k, x['temp_max'], x['temp_min']))
-             
+        reg_ex = re.search('current weather in (.*)', command)
+        if reg_ex:
+            '''
+            city = reg_ex.group(1)
+            owm = OWM(API_key='221642e84fc28ddb1c3172447bbf1a39')
+            obs = owm.weather_at_place(city)
+            w = obs.get_weather()
+            k = w.get_status()
+            x = w.get_temperature(unit='celsius')
+            botResponse('Current weather in %s is %s. The maximum temperature is %0.2f and the minimum temperature is %0.2f degree celcius' % (city, k, x['temp_max'], x['temp_min']))
+
+            city=reg_ex.group(1)
+            url="https://api.openweathermap.org/data/2.5/forecast?q=Delhi&appid=221642e84fc28ddb1c3172447bbf1a39&units=metric"
+            res=requests.get(url)
+            output=res.json()
+            '''
+            city = reg_ex.group(1)
+            
+            url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3'%city
+            response = requests.get(url)
+            response.raise_for_status()
+
+            weather_status=output['weather'][0]['description']
+            temprature=output['main']['temp']
+            output = response.json()
+            #humidity=output['main']['humidity']
+            #wind_speed=output['wind']['speed']
+            botResponse('Current weather stauts is' + weather_status)
+            botResponse('Current temparture is ' + str(temprature))
+
     #tells you the information availabe in net
     elif 'tell me about' in command:
         reg_ex = re.search('tell me about (.*)', command)
@@ -222,11 +303,21 @@ def assistant(command):
                 os.chdir(path)
                 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                     ydl.download([url])
-                p = subprocess.Popen(path)
+                    subprocess.Popen(path)
                 if flag == 0:
                     botResponse('I have not found anything in Youtube ')
-                    
-                    
+                else:
+                    pass
+    #tell you a joke
+    elif 'joke' in command:
+        res = requests.get(
+               'https://icanhazdadjoke.com/',
+                headers={"Accept":"application/json"})
+        if res.status_code == requests.codes.ok:
+            botResponse(str(res.json()['joke']))
+        else:       
+            botResponse('Oops! I ran out of jokes')
+
     #read the news
     elif 'news for today' in command:
             try:
@@ -240,22 +331,33 @@ def assistant(command):
                     botResponse(news.title.text.encode('utf-8'))
             except Exception as e:
                     print(e)
-    
+
+    elif "calculate" in command:    
+            # write your wolframalpha app_id here 
+            app_id = "9Q8RQA-XK2UTE7ALR" 
+            client = wolframalpha.Client(app_id) 
+  
+            indx = input.lower().split().index('calculate') 
+            query = input.split()[indx + 1:] 
+            res = client.query(' '.join(query)) 
+            answer = next(res.results).text 
+            botResponse('The answer is ' + answer) 
+
     #does a google search
     elif 'search' in command:
         botResponse('What to search?')
         #myCommand()
 
-        q=sr.Recognizer()
+        w=sr.Recognizer()
         t=0
 
         with sr.Microphone() as source:
             print('Search for the term:')
             
             while t==0:
-                audio =q.listen(source)
+                audio = w.listen(source)
                 try:
-                    text =q.recognize_google(audio)
+                    text =w.recognize_google(audio)
                     print('you said :{}'.format(text))
                     t=1
 
@@ -269,11 +371,18 @@ def assistant(command):
         safari_path = r'/Applications/Safari.app %s'
         for url in search(query, tld="co.in", num=1, stop = 1, pause = 2):
             webbrowser.open("https://google.com/search?q=%s" % query)
+
             
     #to terminate the program
     elif 'bye' in command:
-         botResponse('Bye bye Sir. Have a nice day')
-         sys.exit()
+        botResponse('Bye bye Sir. Have a nice day')
+        sys.exit()
+
+    else:
+        response = chatbot.get_response(command)
+        botResponse('response')
+
+        
        
 '''         
 day_time = int(strftime('%H'))
@@ -284,7 +393,33 @@ elif 12 <= day_time < 18:
 else:
     botResponse('Hello Good evening, Sir')
 '''
-botResponse('Hello, I am the Voice assistant created by Harshit Ruwali')
+chatbot = ChatBot('Lindy')
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
+trainer.export_for_training('./ai.yml')
+trainer.export_for_training('./botprofile.yml')
+trainer.export_for_training('./computers.yml')
+trainer.export_for_training('./conversations.yml')
+trainer.export_for_training('./emotion.yml')
+trainer.export_for_training('./food.yml')
+trainer.export_for_training('./gossip.yml')
+trainer.export_for_training('./greetings.yml')
+trainer.export_for_training('./health.yml')
+trainer.export_for_training('./history.yml')
+trainer.export_for_training('./humor.yml')
+trainer.export_for_training('./literature.yml')
+trainer.export_for_training('./money.yml')
+trainer.export_for_training('./movies.yml')
+trainer.export_for_training('./politics.yml')
+trainer.export_for_training('./psychology.yml')
+trainer.export_for_training('./science.yml')
+trainer.export_for_training('./sports.yml')
+trainer.export_for_training('./trivia.yml')
+
+print('traing is finished')
+
+
+botResponse('Hello, I am the Voice assistant created by Harshit Ruwali. How can I help you?')
 
 while True:
     assistant(myCommand())
